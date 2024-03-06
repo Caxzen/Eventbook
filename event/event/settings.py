792a59ecc16938a0029ed0ca20367435a25e04cd
Ajay <84972015+Caxzen@users.noreply.github.com>
2024-03-07 01:10:35 +0530

    Update settings.py

--- event/event/settings.py
+++ event/event/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'caxzenofficial@gmail.com'
-EMAIL_HOST_PASSWORD = 'snap vbrr jreb mlnb'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
